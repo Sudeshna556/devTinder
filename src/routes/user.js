f449afbaabc7b09a30bbda8abbaa6066daff21c6
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2025-08-04 00:42:29 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const userRouter = express.Router(); 
 const { userAuth } = require("../middlewares/auth"); 
-const ConnectionRequest = require("../models/connectionRequest"); 
+const ConnectionRequest = require("../models/connectionRequest");  // Importing the ConnectionRequest model
+const User = require("../models/user"); // Importing the User model
 
+
+
+
+//check all the pending/received connection requests 
 userRouter.get("/check-requests/all-received-requests", userAuth, async (req, res) => {
     try {
         // Extracting the logged-in user's ID
@@ -33,5 +38,77 @@ userRouter.get("/check-requests/all-received-requests", userAuth, async (req, re
 });
 
 
+//check all the sent connection requests to others
+userRouter.get("/users/connections",userAuth, async(req,res) =>{
+    try{
+        const loggedInUserId = req.user; // Get the logged-in user's ID
+
+        const userConnetions = await ConnectionRequest.find({
+            $or: [
+                { SenderId: loggedInUserId._id, status :"accepted" }, // Requests sent by the user
+                { ReceiverId: loggedInUserId._id, status :"accepted"  }, // Requests received by the user
+            ]
+        }).populate("SenderId", ["name", "email", "profilePicture", "gender", "skills", "about"])
+        .populate("ReceiverId",["name", "email", "profilePicture", "gender", "skills", "about"]) // Populate senderId with user details
+
+        // If no connections found, return an empty array
+        if(userConnetions.length === 0){
+            return res.status(200).json({ message: "No connections found", data: [] });
+        }
+        // Return the user connections
+       const data = userConnetions.map((row)=>{
+        if(row.SenderId._id.toString() === loggedInUserId._id.toString()){
+         return row.ReceiverId
+        }
+         return row.SenderId; // Return the other user's details
+       })
+       
+
+       res.json({data});
+    }catch(err){
+        res.status(400).send("Error in fetching user connections");
+    }
+});
+
+//check all profiles except those whom the user has already sent connection requests and those who are already connected or rejected
+userRouter.get("/users/feed", userAuth, async(req,res) => { // /users/feed?page=1&limit=50
+    // check for logged in user
+    const loggedInUserId = req.user;
+    const page = parseInt(req.query.page) || 1; // Get the skip value from query params, default to 0
+    let limit = parseInt(req.query.limit) || 10; // Get the limit value from query params, default to 10
+    limit = limit>50 ? 50 : limit; // Limit the maximum number of results to 50
+
+    const skip = (page-1) *limit; // Calculate the skip value for pagination
+    
+
+    // find all the connection requests sent by the logged-in user
+    const sendRequests = await ConnectionRequest.find({
+        $or:[
+            {SenderId: loggedInUserId._id}, // Requests sent by the user
+            {ReceiverId: loggedInUserId._id} // Requests received by the user
+        ]
+    }).select("fromUserId   toUserId");
+
+    //hide those users from the feed who have already sent connection requests or are already connected or rejected
+    const hideUsersfromFeed = new Set();
+    sendRequests.forEach((req) => {
+        hideUsersfromFeed.add(req.ReceiverId.toString());
+        hideUsersfromFeed.add(req.SenderId.toString());
+    });
+    console.log("Hide users from feed:", hideUsersfromFeed);
+
+    const users = await User .find({
+        $and:
+         [
+                { _id: { $ne: loggedInUserId._id } }, // Exclude the logged-in user
+                { _id: { $nin: Array.from(hideUsersfromFeed) } } // Exclude users already sent requests or connected
+        ]           
+    })
+    .select("name email profilePicture gender skills  about" )
+    .skip(skip)
+    .limit(limit); // Select only the necessary fields
+
+    res.send(users);;
+});
 
 module.exports = userRouter; // Export the userRouter to use in other files
