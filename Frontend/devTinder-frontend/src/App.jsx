1da304f503a78aae67f54c12b82df2c0332df77b
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2025-08-05 00:34:37 +0530

    Navbar

--- Frontend/devTinder-frontend/src/App.jsx
+++ Frontend/devTinder-frontend/src/App.jsx
@@ -1,13 +1,12 @@
-
+import Navbar from "./Navbar"
 
 
 function App() {
-
-
   return (
     <>
       <div>
-        <h1 className="h-6 font-bold text-blue-600">hello world!!</h1>
+        {/* <h1 className="h-6 font-bold text-blue-600">hello world!!</h1> */}
+      <Navbar/>
       </div>
     </>
   )
