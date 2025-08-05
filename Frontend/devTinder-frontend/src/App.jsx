337f3ab135880f6e2a9fd02b954a4f3a2a5d62b8
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2025-08-05 21:53:51 +0530

    creating components

--- Frontend/devTinder-frontend/src/App.jsx
+++ Frontend/devTinder-frontend/src/App.jsx
@@ -1,12 +1,27 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./Body"
 import Navbar from "./Navbar"
+import Login from "./Login"
 
 
 function App() {
   return (
     <>
       <div>
-        {/* <h1 className="h-6 font-bold text-blue-600">hello world!!</h1> */}
-      <Navbar/>
+        <BrowserRouter>
+        <Routes>
+          {/* rendering body component, and inside routing my login component */}
+          <Route path="/" element={<Body/>} > 
+            <Route path = "login" element = {<Login/>}/>
+          </Route>
+          <Route path="/login" element={<h1 className="h-6 font-bold text-blue-600">Login Page</h1>} />
+          <Route path="/signup" element={<h1 className="h-6 font-bold text-blue-600">Signup Page</h1>} />
+                 
+        </Routes>
+        </BrowserRouter>
+       
+      {/* <Navbar/> */}
       </div>
     </>
   )
