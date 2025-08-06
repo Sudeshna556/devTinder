89a8306ceb33e219d1c3129f3cd89d20c60b963f
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2025-08-07 01:03:20 +0530

    login comonent

--- Frontend/devTinder-frontend/src/Login.jsx
+++ Frontend/devTinder-frontend/src/Login.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    //     <div className="flex justify-center items-center ">
+    // <div className="card  bg-neutral text-neutral-content w-96 shadow-sm ">
+    //   <div className="card-body">
+    //     <h2 className="card-title">Login</h2>
+    //     <fieldset className="fieldset">
+    //   <legend className="fieldset-legend">Page title</legend>
+    //   <input type="text" className="input" placeholder="My awesome page" />
 
-export default Login
+    // </fieldset>
+
+    //     <fieldset className="fieldset">
+    //   <legend className="fieldset-legend">Page title</legend>
+    //   <input type="text" className="input" placeholder="My awesome page" />
+
+    // </fieldset>
+
+    //     <div className="card-actions justify-end">
+    //       <button className="btn btn-primary ">Buy Now</button>
+    //     </div>
+    //   </div>
+    // </div>
+    //     </div>
+
+    <div className="card card-side shadow-sm bg-neutral-900 w-4xl h-full mx-auto mt-10">
+      <figure>
+        <img
+          className="w-96 h-xl object-cover"
+          src="../src/assets/login-bg.jpg"
+          alt="Movie"
+        />
+      </figure>
+      <div className="card-body ml-14">
+        <h2 className="card-title justify-center mr-16">Login</h2>
+        <p className="card-sm justify-center ml-32">Welcome Back.</p>
+        <hr className="mt-2 mb-4 w-84 " />
+        <div className="mt-4 ">
+          <h2 className="mb-2.5 ">Email</h2>
+          <input
+            className="input validator"
+            type="email"
+            required
+            placeholder="mail@site.com"
+          />
+        </div>
+
+        <div className="mt-4">
+          <h2 className="mb-2.5 ">Password</h2>
+          <input
+            type="password"
+            className="input validator"
+            required
+            placeholder="Password"
+            minlength="8"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+          <p className="validator-hint">
+            Must be more than 8 characters, including
+            <br />
+            At least one number
+            <br />
+            At least one lowercase letter
+            <br />
+            At least one uppercase letter
+          </p>
+        </div>
+        <div className="card-actions justify-center mr-14 mb-3 -mt-4">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
