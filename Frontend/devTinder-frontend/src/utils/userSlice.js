61f2abb83d4762fb0f4da23186b0aa687980d28a
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2025-08-09 01:42:48 +0530

    configure redux

---
+++ Frontend/devTinder-frontend/src/utils/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    name : user,
+    intiateState : null,
+    reducers : {
+        addUser : (state, action) => {
+            return action.payload; // This will update the state with the user data
+        },
+        removeUser : (state, action) => {
+            return null; // This will reset the state to null when the user logs out
+        },
+    }
+})
+
+export const { addUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
